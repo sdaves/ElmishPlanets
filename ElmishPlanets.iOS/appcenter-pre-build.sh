5c54bf257acae7d1fa75fd4d0f61d41a45713d9a
Stephen Daves <contact@stephendaves.com>
2019-01-03 15:33:12 -0500

    Update appcenter-pre-build.sh

--- ElmishPlanets.iOS/appcenter-pre-build.sh
+++ ElmishPlanets.iOS/appcenter-pre-build.sh
@@ -2,5 +2,5 @@ echo Update Info.plist
 
 line="\<string\>com.timothelariviere.elmishplanets\<\/string\>"
 rep="\<string\>${CFBundleIdentifier}\<\/string\>"
-sed -i "s/${line}/${rep}/g" ./Info.plist
+sed -i "s/${line}/${rep}/g" Info.plist
 cat ./Info.plist
